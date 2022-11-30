f22df7df5680aaabdb700ea7458719cff176a05f
hajubal <hajubal@gmail.com>
2022-12-01 00:36:02 +0900

    initial commit

---
+++ src/main/java/me/synology/hajubal/coins/PickUpCoinsApplication.java
@@ -0,0 +1,13 @@
+package me.synology.hajubal.coins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickUpCoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickUpCoinsApplication.class, args);
+	}
+
+}
