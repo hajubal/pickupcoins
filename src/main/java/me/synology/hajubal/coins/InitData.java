69c0f965f959d7fd46a8c9173e1333d4e97098f2
hajubal <hajubal@gmail.com>
2022-12-08 00:00:00 +0900

    client site link repository 적용 중

--- src/main/java/me/synology/hajubal/coins/InitData.java
+++ src/main/java/me/synology/hajubal/coins/InitData.java
@@ -10,6 +10,10 @@ import me.synology.hajubal.coins.respository.PointUrlRepository;
 import me.synology.hajubal.coins.service.NaverPointService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import java.io.FileReader;
+import java.nio.CharBuffer;
 
 @Component
 public class InitData {
@@ -28,6 +32,17 @@ public class InitData {
 
     @PostConstruct
     public void init() {
+        CharBuffer buffer = CharBuffer.allocate(10000);
+
+
+        try (FileReader fileReader = new FileReader("C:\\Users\\hajubal\\IdeaProjects\\pickupcoins\\tmp\\cookie.txt")){
+            fileReader.read(buffer);
+
+            System.out.println(">>>"+buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         String COOKIE = "";
 
         CookieData cookieData = CookieData.builder().id(1l).cookie(COOKIE).build();
@@ -36,8 +51,8 @@ public class InitData {
 
         siteRepository.save(SiteData.builder().name("클리앙").url("https://www.clien.net/service/board/jirum").build());
 
-        pointUrlRepository.save(PointUrlData.builder().url("https://campaign2-api.naver.com/click-point/?eventId=cr_2022120103_2212_1_1109").build());
+        //pointUrlRepository.save(PointUrlData.builder().url("https://campaign2-api.naver.com/click-point/?eventId=cr_2022120103_2212_1_1109").build());
 
-        naverPointService.savePoint();
+//        naverPointService.savePoint();
     }
 }
--- src/test/java/me/synology/hajubal/coins/PickUpCoinsApplicationTests.java
+++ src/test/java/me/synology/hajubal/coins/PickUpCoinsApplicationTests.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileReader;
+import java.nio.CharBuffer;
+
 @SpringBootTest
 class PickUpCoinsApplicationTests {
 
@@ -14,6 +17,20 @@ class PickUpCoinsApplicationTests {
 	@Test
 	void contextLoads() {
 		pointService.run();
+	}
+
+	@Test
+	void filereadTest() {
+		CharBuffer buffer = CharBuffer.allocate(10000);
+
+
+		try (FileReader fileReader = new FileReader("C:\\Users\\hajubal\\IdeaProjects\\pickupcoins\\tmp\\cookie.txt")){
+			fileReader.read(buffer);
+
+			System.out.println(">>>"+buffer);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 
