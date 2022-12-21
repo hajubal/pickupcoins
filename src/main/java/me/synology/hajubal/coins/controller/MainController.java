bcce9b71daf93f44ea8570e190f222b5a6aa1ecf
hajubal <hajubal@gmail.com>
2022-12-21 22:13:07 +0900

    Feat: main page 작업중

--- src/main/java/me/synology/hajubal/coins/controller/MainController.java
+++ src/main/java/me/synology/hajubal/coins/controller/MainController.java
@@ -1,11 +1,20 @@
 package me.synology.hajubal.coins.controller;
 
 import me.synology.hajubal.coins.controller.dto.CookieUpdateDto;
+import me.synology.hajubal.coins.entity.PointUrlUserCookie;
+import me.synology.hajubal.coins.entity.UserCookie;
+import me.synology.hajubal.coins.respository.PointUrlUserCookieRepository;
+import me.synology.hajubal.coins.respository.UserCookieRepository;
 import me.synology.hajubal.coins.service.UserCookieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -13,8 +22,29 @@ public class MainController {
     @Autowired
     private UserCookieService userCookieService;
 
+    @Autowired
+    private UserCookieRepository userCookieRepository;
+
+    @Autowired
+    private PointUrlUserCookieRepository pointUrlUserCookieRepository;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        List<PointUrlUserCookie> all = pointUrlUserCookieRepository.findAll();
+
+        model.addAttribute("items", all);
+
+        return "index";
+    }
+
+    @GetMapping("/updateCookie/{userId}")
+    public String updateCookieView(@PathVariable Long userId, Model model) {
+
+        UserCookie userCookie = userCookieRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Not found user."));
+
+        model.addAttribute("userCookie", userCookie);
+
         return "cookieDetail";
     }
 
