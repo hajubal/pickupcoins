67cef27d3516631a0354ba9615f9c81c7df2afc1
hajubal <hajubal@gmail.com>
2023-02-05 22:42:10 +0900

    Refactor: point save 리펙토링 중

--- src/main/java/me/synology/hajubal/coins/respository/PointUrlUserCookieRepository.java
+++ src/main/java/me/synology/hajubal/coins/respository/PointUrlUserCookieRepository.java
@@ -1,6 +1,8 @@
 package me.synology.hajubal.coins.respository;
 
+import me.synology.hajubal.coins.entity.PointUrl;
 import me.synology.hajubal.coins.entity.PointUrlUserCookie;
+import me.synology.hajubal.coins.entity.UserCookie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface PointUrlUserCookieRepository extends JpaRepository<PointUrlUserCookie, Long> {
 
     Optional<PointUrlUserCookie> findByPointUrl_UrlAndUserCookieUserName(String pointUrl, String userName);
+
+    Optional<PointUrlUserCookie> findByPointUrlAndUserCookie(PointUrl pointUrl, UserCookie userCookie);
 }
--- src/main/java/me/synology/hajubal/coins/service/NaverPointService.java
+++ src/main/java/me/synology/hajubal/coins/service/NaverPointService.java
@@ -3,7 +3,9 @@ package me.synology.hajubal.coins.service;
 import lombok.extern.slf4j.Slf4j;
 import me.synology.hajubal.coins.entity.*;
 import me.synology.hajubal.coins.respository.*;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.NullableUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -74,36 +76,16 @@ public class NaverPointService {
             }
 
             //TODO 쿼리 한번에 데이터를 가져오도록 수정.
-            //이미 접속한 URL인 경우 제외
-            if(!url.getPermanent() && pointUrlUserCookieRepository.findByPointUrl_UrlAndUserCookieUserName(url.getUrl(), userCookie.getUserName()).isPresent()) return;
+            //사용자가 이미 접속한 URL인 경우 제외
+            if(!url.getPermanent() && pointUrlUserCookieRepository.findByPointUrlAndUserCookie(url, userCookie).isPresent()) {
+                log.info("이미 수집된 URL. url: {}, user: {}", url.getUrl(), userCookie.getUserName());
+                return;
+            }
 
             log.info("call point url: {}. user name: {}", url.getUrl(), userCookie.getUserName());
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Cookie", userCookie.getCookie());
-            headers.add("user-agent", "/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
-
             try {
-                ResponseEntity<String> response =  new RestTemplate().exchange(url.getUrl(), GET, new HttpEntity<String>(headers), String.class);
-
-                if(response.getBody().contains("포인트 지급을 위해서는 로그인이 필요합니다")) {
-                    userCookie.setIsValid(false);
-
-                    log.info("로그인이 풀린 사용자: {}, 사이트: {}, cookie: {}", userCookie.getUserName(), userCookie.getSiteName(), userCookie.getCookie());
-
-                    slackService.sendMessage("[ " + userCookie.getUserName() + " ] 로그인 풀림.");
-                } else if(response.getBody().contains("적립")) {
-                    savedPointRepository.save(SavedPoint.builder()
-                            .point("코드 수정 필요")
-                            .userCookie(userCookie)
-                            .build());
-                }
-
-                //cookie session값 갱신
-                if(response.getHeaders().containsKey("cookie")) {
-                    log.info("cookie 갱신 user: {}", userCookie.getUserName());
-                    userCookie.setCookie(response.getHeaders().getFirst("cookie"));
-                }
+                ResponseEntity<String> response = getResponseEntity(url, userCookie);
 
                 //사용자 별 호출 url 정보 저장
                 pointUrlUserCookieRepository.save(PointUrlUserCookie.builder()
@@ -135,5 +117,43 @@ public class NaverPointService {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    @NotNull
+    private ResponseEntity<String> getResponseEntity(PointUrl url, UserCookie userCookie) {
+        ResponseEntity<String> response =  new RestTemplate().exchange(url.getUrl(), GET, new HttpEntity<String>(getHeaders(userCookie)), String.class);
+
+        if(response.getBody() == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        if(response.getBody().contains("포인트 지급을 위해서는 로그인이 필요합니다")) {
+            userCookie.setIsValid(false);
+
+            log.info("로그인이 풀린 사용자: {}, 사이트: {}, cookie: {}", userCookie.getUserName(), userCookie.getSiteName(), userCookie.getCookie());
+
+            slackService.sendMessage("[ " + userCookie.getUserName() + " ] 로그인 풀림.");
+        } else if(response.getBody().contains("적립")) {
+            savedPointRepository.save(SavedPoint.builder()
+                    .point("코드 수정 필요")
+                    .userCookie(userCookie)
+                    .build());
+        }
+
+        //cookie session값 갱신
+        if(response.getHeaders().containsKey("cookie")) {
+            log.info("cookie 갱신 user: {}", userCookie.getUserName());
+            userCookie.setCookie(response.getHeaders().getFirst("cookie"));
+        }
+
+        return response;
+    }
+
+    @NotNull
+    private static HttpHeaders getHeaders(UserCookie userCookie) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cookie", userCookie.getCookie());
+        headers.add("user-agent", "/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
+        return headers;
     }
 }
