96d0dec40bec499fa3a44b7acc3b83efdccc5230
hajubal <hajubal@gmail.com>
2023-02-28 23:38:37 +0900

    Feat: thymeleaf spring security extras 버전 수정

--- src/main/java/me/synology/hajubal/coins/conf/SecurityConfig.java
+++ src/main/java/me/synology/hajubal/coins/conf/SecurityConfig.java
@@ -13,10 +13,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/insertCookie2")
-                        .permitAll()
-                        .anyRequest().authenticated()
-                );
         http.formLogin();
 
         return http.build();
