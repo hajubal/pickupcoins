b9e4b0ae21030654893143c5ee6b9d0603e4a0c8
hajubal <hajubal@gmail.com>
2023-03-06 21:52:19 +0900

    Refactor: web crawler refactoring

--- src/main/java/me/synology/hajubal/coins/crawler/impl/RuliwebCrawler.java
+++ src/main/java/me/synology/hajubal/coins/crawler/impl/RuliwebCrawler.java
@@ -6,6 +6,7 @@ import me.synology.hajubal.coins.entity.type.POINT_URL_TYPE;
 import me.synology.hajubal.coins.entity.PointUrl;
 import me.synology.hajubal.coins.entity.Site;
 import me.synology.hajubal.coins.respository.SiteRepository;
+import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -14,7 +15,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -34,16 +37,46 @@ public class RuliwebCrawler implements WebCrawler {
      */
     @Transactional
     @Override
-    public Set<PointUrl> crawling() {
-        Site site = siteRepository.findByName("루리웹").orElseThrow(() -> new IllegalArgumentException("Not found site name. 루리웹"));
+    public Set<PointUrl> crawling() throws IOException {
+        Optional<Site> site = siteRepository.findByName("루리웹");
 
-        Document document = null;
-
-        try {
-            document = Jsoup.connect(site.getUrl()).get();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        if (site.isEmpty()) {
+            return Collections.emptySet();
         }
+
+        return fetchPointUrls(site.get().getDomain(), fetchPostUrls(site.get().getUrl()));
+    }
+
+    @NotNull
+    private static Set<PointUrl> fetchPointUrls(String domain, Set<String> pointPostUrl) throws IOException {
+        //포인트 url
+        Set<PointUrl> pointUrl = new HashSet<>();
+
+        for (String url : pointPostUrl) {
+            Document postDocument = Jsoup.connect(domain + url).get();
+
+            //게시글 내용에 링크
+            postDocument.select(".board_main_view a").forEach(aTag -> {
+                String pointHref = aTag.attr("href");
+
+                if(pointHref.contains("campaign2-api.naver.com")) {
+                    log.debug("Naver point url: {}", pointHref);
+
+                    pointUrl.add(PointUrl.builder().pointUrlType(POINT_URL_TYPE.NAVER).url(pointHref).name("ruliweb").build());
+                } else if(pointHref.contains("ofw.adison.co/u/naverpay")) {
+                    log.debug("Adison naver point url: {}", pointHref);
+
+                    pointUrl.add(PointUrl.builder().pointUrlType(POINT_URL_TYPE.OFW_NAVER).url(pointHref).name("ruliweb").build());
+                }
+            });
+        }
+
+        return pointUrl;
+    }
+
+    @NotNull
+    private static Set<String> fetchPostUrls(String siteUrl) throws IOException {
+        Document document = Jsoup.connect(siteUrl).get();
 
         Set<String> pointPostUrl = new HashSet<>();
 
@@ -60,36 +93,7 @@ public class RuliwebCrawler implements WebCrawler {
                 pointPostUrl.add(href);
             }
         });
-
-        //포인트 url
-        Set<PointUrl> pointUrl = new HashSet<>();
-
-        pointPostUrl.forEach(url -> {
-            Document postDocument;
-
-            try {
-                postDocument = Jsoup.connect(site.getDomain() + url).get();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            //게시글 내용에 링크
-            postDocument.select(".board_main_view a").forEach(aTag -> {
-                String pointHref = aTag.attr("href");
-
-                if(pointHref.contains("campaign2-api.naver.com")) {
-                    log.debug("Naver point url: {}", pointHref);
-
-                    pointUrl.add(PointUrl.builder().pointUrlType(POINT_URL_TYPE.NAVER).url(pointHref).name("ruliweb").build());
-                } else if(pointHref.contains("ofw.adison.co/u/naverpay")) {
-                    log.debug("Adison naver point url: {}", pointHref);
-
-                    pointUrl.add(PointUrl.builder().pointUrlType(POINT_URL_TYPE.OFW_NAVER).url(pointHref).name("ruliweb").build());
-                }
-            });
-        });
-
-        return pointUrl;
+        return pointPostUrl;
     }
 
     @Override
