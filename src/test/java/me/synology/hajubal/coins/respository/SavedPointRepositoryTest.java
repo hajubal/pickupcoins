6e55f5516fa4eb00c7a29e70f5370a57af3c76d9
hajubal <hajubal@gmail.com>
2023-04-11 21:56:35 +0900

    Test: delete test 작성중

--- src/test/java/me/synology/hajubal/coins/respository/SavedPointRepositoryTest.java
+++ src/test/java/me/synology/hajubal/coins/respository/SavedPointRepositoryTest.java
@@ -1,13 +1,17 @@
 package me.synology.hajubal.coins.respository;
 
+import me.synology.hajubal.coins.entity.PointUrlUserCookie;
 import me.synology.hajubal.coins.entity.SavedPoint;
 import me.synology.hajubal.coins.entity.UserCookie;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -32,5 +36,22 @@ class SavedPointRepositoryTest {
         List<UserCookie> all = userCookieRepository.findAll();
 
         all.forEach(System.out::println);
+    }
+
+    @Test
+    void deleteTest() {
+        UserCookie userCookie = UserCookie.builder().userName("test").siteName("test").isValid(Boolean.TRUE).build();
+        userCookie.setSavedPoint(List.of(SavedPoint.builder().userCookie(userCookie).build()));
+        userCookie.setPointUrlUserCookie(List.of(PointUrlUserCookie.builder().userCookie(userCookie).build()));
+
+        userCookieRepository.save(userCookie);
+
+        //?? 이게 왜 주소값이 다르지???
+        assertThat(userCookie).isEqualTo(userCookieRepository.findById(userCookie.getId()).get());
+
+//        assertThat(userCookie.getSavedPoint());
+
+
+
     }
 }
