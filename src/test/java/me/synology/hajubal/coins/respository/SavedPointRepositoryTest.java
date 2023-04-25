ddf1f579fe736ae743e2eb20e5d1b70ca0def3c8
hajubal <hajubal@gmail.com>
2023-04-25 21:10:13 +0900

    Refactor: test 오류 수정

--- src/test/java/me/synology/hajubal/coins/respository/SavedPointRepositoryTest.java
+++ src/test/java/me/synology/hajubal/coins/respository/SavedPointRepositoryTest.java
@@ -4,17 +4,18 @@ import me.synology.hajubal.coins.entity.PointUrlUserCookie;
 import me.synology.hajubal.coins.entity.SavedPoint;
 import me.synology.hajubal.coins.entity.UserCookie;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
-@SpringBootTest
 class SavedPointRepositoryTest {
 
     @Autowired
