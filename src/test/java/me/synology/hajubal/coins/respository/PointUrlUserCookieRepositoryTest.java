43ff8b7a9782763b7389fb147067fc83a7f6bc53
hajubal <hajubal@gmail.com>
2023-05-03 21:37:03 +0900

    Test: test case 추가

---
+++ src/test/java/me/synology/hajubal/coins/respository/PointUrlUserCookieRepositoryTest.java
@@ -0,0 +1,33 @@
+package me.synology.hajubal.coins.respository;
+
+import me.synology.hajubal.coins.entity.PointUrl;
+import me.synology.hajubal.coins.entity.PointUrlUserCookie;
+import me.synology.hajubal.coins.entity.UserCookie;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class PointUrlUserCookieRepositoryTest {
+
+    @Autowired
+    private PointUrlUserCookieRepository pointUrlUserCookieRepository;
+
+    @Test
+    void saveTest() {
+        PointUrl pointUrl = PointUrl.builder().url("url").name("name").build();
+        UserCookie userCookie = UserCookie.builder().cookie("cookie").userName("name").siteName("site").isValid(Boolean.TRUE).build();
+
+        PointUrlUserCookie pointUrlUserCookie = PointUrlUserCookie.builder().pointUrl(pointUrl).userCookie(userCookie).build();
+
+        PointUrlUserCookie savedData = pointUrlUserCookieRepository.save(pointUrlUserCookie);
+
+        Assertions.assertThat(pointUrlUserCookie).isEqualTo(savedData);
+    }
+}
