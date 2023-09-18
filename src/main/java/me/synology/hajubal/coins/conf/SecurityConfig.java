1cbb8fae202cbcc438be6736041ffceead776266
hajubal <hajubal@gmail.com>
2023-09-18 15:10:56 +0900

    Fix: h2를 사용하지 않는 profile인 경우 오류 발생 패치
    h2-console이 propertiesdp 설정이 안될 경우 오류 발생

--- src/main/java/me/synology/hajubal/coins/conf/SecurityConfig.java
+++ src/main/java/me/synology/hajubal/coins/conf/SecurityConfig.java
@@ -2,6 +2,8 @@ package me.synology.hajubal.coins.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,13 +14,22 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Profile("local")
+    @Order(0)
+    @Bean
+    public SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests().requestMatchers(toH2Console()).permitAll().and().build();
+    }
+
+    @Order(1)
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .cors().disable()
                 .headers().frameOptions().disable().and()
-                .authorizeHttpRequests().requestMatchers(toH2Console()).permitAll()
+                .authorizeHttpRequests()
+                //.requestMatchers(toH2Console()).permitAll()  //h2-console이 propertiesdp 설정이 안될 경우 오류 발생
                 .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 .and().formLogin()
