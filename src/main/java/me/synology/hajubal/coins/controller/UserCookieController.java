c9c8596ab74bdbef8bfc875780d5afb795e341dc
hajubal <hajubal@gmail.com>
2024-02-21 13:15:06 +0900

    Refactor: cookieUser -> userCookie 폴더명 변경

--- src/main/java/me/synology/hajubal/coins/controller/UserCookieController.java
+++ src/main/java/me/synology/hajubal/coins/controller/UserCookieController.java
@@ -25,7 +25,7 @@ public class UserCookieController {
         List<UserCookie> list = userCookieService.getAll();
         model.addAttribute("items", list);
 
-        return "cookieUser/users";
+        return "userCookie/users";
     }
 
     @GetMapping("/user/{userCookieId}")
@@ -33,7 +33,7 @@ public class UserCookieController {
         UserCookie userCookie = userCookieService.getUserCookie(userCookieId);
         model.addAttribute("userCookie", userCookie);
 
-        return "cookieUser/editUser";
+        return "userCookie/editUser";
     }
 
     @PostMapping("/user/{userId}")
@@ -41,7 +41,7 @@ public class UserCookieController {
             , BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
-            return "cookieUser/editUser";
+            return "userCookie/editUser";
         }
 
         userCookieService.updateUserCookie(userId, cookieUpdateDto);
@@ -53,7 +53,7 @@ public class UserCookieController {
     public String insertUser(Model model) {
         model.addAttribute("userCookie", new CookieDto.CookieInsertDto());
 
-        return "cookieUser/addUser";
+        return "userCookie/addUser";
     }
 
     @PostMapping("/insertUser")
@@ -61,7 +61,7 @@ public class UserCookieController {
             , BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
-            return "cookieUser/addUser";
+            return "userCookie/addUser";
         }
 
         Long userId = userCookieService.insertUserCookie(cookieInsertDto);
