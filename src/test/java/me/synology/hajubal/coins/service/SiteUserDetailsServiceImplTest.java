5190fe4793217affbd412c5c115204f935d80e2f
hajubal <hajubal@gmail.com>
2024-02-24 13:05:38 +0900

    Test: 사용자 조회 test case 추가

--- src/test/java/me/synology/hajubal/coins/service/SiteUserDetailsServiceImplTest.java
+++ src/test/java/me/synology/hajubal/coins/service/SiteUserDetailsServiceImplTest.java
@@ -1,17 +1,23 @@
 package me.synology.hajubal.coins.service;
 
+import me.synology.hajubal.coins.entity.SiteUser;
+import me.synology.hajubal.coins.respository.SiteUserRepository;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -22,7 +28,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class SiteUserDetailsServiceImplTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private SiteUserRepository userRepository;
 
     @Disabled
     @DisplayName("사용자 비밀번호 암호화.")
@@ -35,19 +44,16 @@ class SiteUserDetailsServiceImplTest {
     }
 
     @Test
-    void unauthorizedTest() throws Exception {
-        this.mockMvc.perform(get("/"))
-                .andExpect(status().isUnauthorized());
+    void findUser() {
+        //given
+        userRepository.save(SiteUser.builder().loginId("user").password("password").userName("userName").build());
+
+        //when
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername("user");
+
+        //then
+        assertThat(userDetails.getUsername()).isEqualTo("user");
     }
 
-//    @WithUserDetails("user@example.com")
-    @WithMockUser
-    @Test
-    void loginTest() throws Exception {
-        this.mockMvc.perform(get("/"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/dashboard"))
-        ;
-    }
 
 }
