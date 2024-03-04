ed1682c2cd5572e74b54a397fb00cbb33c3f3d29
hajubal <hajubal@gmail.com>
2024-03-04 20:53:27 +0900

    Test: integTest 추가
    profile 문제 확인 필요.

---
+++ src/integTest/java/me/synology/hajubal/coins/PickUpCoinsLoginTest.java
@@ -0,0 +1,46 @@
+package me.synology.hajubal.coins;
+
+import me.synology.hajubal.coins.pages.HomePage;
+import me.synology.hajubal.coins.pages.LoginPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+//FIXME test profile 사용시 오류 발생
+@ActiveProfiles("local")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PickUpCoinsLoginTest {
+
+    private WebDriver driver;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setup() {
+        this.driver = new HtmlUnitDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.driver.quit();
+    }
+
+    @Test
+    void login() {
+        final LoginPage loginPage = HomePage.to(this.driver, this.port);
+        loginPage.assertAt();
+
+        HomePage homePage = loginPage.loginForm().username("user").password("user").submit();
+        homePage.assertAt();
+
+        LoginPage logoutSuccess = homePage.logout();
+        logoutSuccess.assertAt();
+    }
+
+}
