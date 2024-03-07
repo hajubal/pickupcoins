c773c2029753abea56aa8aac68023771b40ffab9
hajubal <hajubal@gmail.com>
2024-03-07 21:24:06 +0900

    Feat: page server 로직 추가

--- src/main/java/me/synology/hajubal/coins/controller/PointController.java
+++ src/main/java/me/synology/hajubal/coins/controller/PointController.java
@@ -2,18 +2,16 @@ package me.synology.hajubal.coins.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.synology.hajubal.coins.entity.PointUrl;
 import me.synology.hajubal.coins.entity.PointUrlCallLog;
 import me.synology.hajubal.coins.respository.PointUrlCallLogRepository;
 import me.synology.hajubal.coins.respository.PointUrlRepository;
 import me.synology.hajubal.coins.schedule.Schedulers;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Collection;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -28,40 +26,29 @@ public class PointController {
 
     @GetMapping("/pointUrl")
     public String pointUrl(Model model) {
-        List<PointUrl> list = pointUrlRepository.findAll();
-
-        model.addAttribute("items", list);
-
+        model.addAttribute("items", pointUrlRepository.findAll());
         return "point/pointUrl";
     }
 
     @GetMapping("/crawling")
     public String crawling() {
         schedulers.webCrawlerScheduler();
-
         return "redirect:/pointUrl";
     }
 
     @GetMapping("/savePoint")
     public String savePoint() {
         schedulers.pointScheduler();
-
         return "redirect:/pointUrl";
     }
 
     @GetMapping("/savePointLog")
-    public String savePointLog(Model model, @RequestParam(name = "userName", defaultValue = "ha") String[] userName) {
+    public String savePointLog(Model model, @RequestParam(defaultValue = "0") int page) {
+        Page<PointUrlCallLog> list = pointUrlCallLogRepository.findAll(PageRequest.of(page, 10));
 
-        List<PointUrlCallLog> list;
-
-        if (userName.length == 0) {
-            list = pointUrlCallLogRepository.findAll();
-        } else {
-            list = pointUrlCallLogRepository.findByUserNameIn(List.of(userName));
-        }
-
-        model.addAttribute("userName", userName);
         model.addAttribute("items", list);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", list.getTotalPages());
 
         return "point/pointLog";
     }
