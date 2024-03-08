b7eceab1e6d69b23c74e6af7432ef7282789c33f
hajubal <hajubal@gmail.com>
2024-03-08 20:21:38 +0900

    Feat: page 화면 추가

--- src/main/java/me/synology/hajubal/coins/controller/PointController.java
+++ src/main/java/me/synology/hajubal/coins/controller/PointController.java
@@ -8,6 +8,9 @@ import me.synology.hajubal.coins.respository.PointUrlRepository;
 import me.synology.hajubal.coins.schedule.Schedulers;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,12 +46,23 @@ public class PointController {
     }
 
     @GetMapping("/savePointLog")
-    public String savePointLog(Model model, @RequestParam(defaultValue = "0") int page) {
-        Page<PointUrlCallLog> list = pointUrlCallLogRepository.findAll(PageRequest.of(page, 10));
+    public String savePointLog(Model model, @PageableDefault(page = 1) Pageable pageable) {
+        int page = pageable.getPageNumber() - 1; // page 위치에 있는 값은 0부터 시작한다.
+
+        Page<PointUrlCallLog> list = pointUrlCallLogRepository.findAll(pageable);
+
+        /**
+         * blockLimit : page 개수 설정
+         * 현재 사용자가 선택한 페이지 앞 뒤로 3페이지씩만 보여준다.
+         * ex : 현재 사용자가 4페이지라면 2, 3, (4), 5, 6
+         */
+        int blockLimit = 10;
+        int startPage = (((int) Math.ceil(((double) pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1;
+        int endPage = Math.min((startPage + blockLimit - 1), list.getTotalPages());
 
         model.addAttribute("items", list);
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", list.getTotalPages());
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
 
         return "point/pointLog";
     }
