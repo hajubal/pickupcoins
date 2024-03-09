e3edaa0a3c5666226eaa52559945ceb682586e07
hajubal <hajubal@gmail.com>
2024-03-09 13:38:12 +0900

    Feat: page 화면 디자인 적용

--- src/main/java/me/synology/hajubal/coins/controller/PointController.java
+++ src/main/java/me/synology/hajubal/coins/controller/PointController.java
@@ -7,14 +7,12 @@ import me.synology.hajubal.coins.respository.PointUrlCallLogRepository;
 import me.synology.hajubal.coins.respository.PointUrlRepository;
 import me.synology.hajubal.coins.schedule.Schedulers;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -46,9 +44,8 @@ public class PointController {
     }
 
     @GetMapping("/savePointLog")
-    public String savePointLog(Model model, @PageableDefault(page = 1) Pageable pageable) {
-        int page = pageable.getPageNumber() - 1; // page 위치에 있는 값은 0부터 시작한다.
-
+    public String savePointLog(Model model, @PageableDefault(page = 0, size = 10, sort = "createdDate"
+            , direction = Sort.Direction.DESC) Pageable pageable) {
         Page<PointUrlCallLog> list = pointUrlCallLogRepository.findAll(pageable);
 
         /**
@@ -56,9 +53,13 @@ public class PointController {
          * 현재 사용자가 선택한 페이지 앞 뒤로 3페이지씩만 보여준다.
          * ex : 현재 사용자가 4페이지라면 2, 3, (4), 5, 6
          */
-        int blockLimit = 10;
-        int startPage = (((int) Math.ceil(((double) pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1;
+        int blockLimit = pageable.getPageSize();
+        int startPage = (((int) Math.ceil(((double) (pageable.getPageNumber() + 1) / blockLimit))) - 1) * blockLimit + 1;
         int endPage = Math.min((startPage + blockLimit - 1), list.getTotalPages());
+
+        log.info("Page number: {}", list.getNumber());
+        log.info("Start page: {}", startPage);
+        log.info("End page: {}", endPage);
 
         model.addAttribute("items", list);
         model.addAttribute("startPage", startPage);
