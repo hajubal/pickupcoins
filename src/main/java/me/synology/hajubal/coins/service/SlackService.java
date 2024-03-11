5bb389e788fc768b24e7ee3f26a31acbf93915de
hajubal <hajubal@gmail.com>
2024-03-11 20:27:31 +0900

    Feat: resilience4j 설정 추가

--- src/main/java/me/synology/hajubal/coins/service/SlackService.java
+++ src/main/java/me/synology/hajubal/coins/service/SlackService.java
@@ -3,12 +3,14 @@ package me.synology.hajubal.coins.service;
 import com.slack.api.Slack;
 import com.slack.api.webhook.Payload;
 import com.slack.api.webhook.WebhookResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.synology.hajubal.coins.exception.SlackConfigException;
 import me.synology.hajubal.coins.exception.SlackServiceException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 
@@ -23,6 +25,10 @@ public class SlackService {
 
     private final Slack slack;
 
+    private static final String DEFAULT_NAME = "hgsssss";
+    private static final String FALLBACK_DEFAULT = "helloFallback";
+
+
     /**
      * 지정된 slack webhook url로 message 전송
      *
@@ -32,6 +38,7 @@ public class SlackService {
      * @throws SlackServiceException 전송 오류
      * @throws SlackConfigException slack 설정 오류
      */
+    @CircuitBreaker(name = DEFAULT_NAME, fallbackMethod = FALLBACK_DEFAULT)
     public WebhookResponse sendMessage(String slackWebhookUrl, String message) throws SlackConfigException, SlackServiceException {
         if(!StringUtils.hasText(slackWebhookUrl)) {
             throw new SlackConfigException("Slack web hook url not set.");
@@ -50,6 +57,12 @@ public class SlackService {
         } catch (IOException e) {
             throw new SlackServiceException(e.getMessage(), e);
         }
+    }
+
+    private WebhookResponse helloFallback(String name, Throwable t){
+        log.error("Fallback : "+ t.getMessage());
+
+        return WebhookResponse.builder().code(500).message("Error for CircuitBreaker").build();
     }
 
 }
