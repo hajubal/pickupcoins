f521106d36db9fdfa5783d7043f35b0135221149
쿠헐헐헐헐 <30306097+hajubal@users.noreply.github.com>
2024-03-18 12:53:27 +0900

    Feat: pointUrlUserCookie 엔티티 이름 변경에 따른 repository 이름 현행화.

--- src/main/java/me/synology/hajubal/coins/respository/PointUrlCookieRepository.java
+++ src/main/java/me/synology/hajubal/coins/respository/PointUrlCookieRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PointUrlUserCookieRepository extends JpaRepository<PointUrlCookie, Long> {
+public interface PointUrlCookieRepository extends JpaRepository<PointUrlCookie, Long> {
 }
