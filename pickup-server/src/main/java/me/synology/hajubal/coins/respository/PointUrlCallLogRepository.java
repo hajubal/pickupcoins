1a3dd7b74bb62e652c76a1e47337f1a149a9ed2b
hajubal <hajubal@gmail.com>
2025-08-12 14:52:30 +0900

    refactor: 공통 모듈 분리에 따른 중복 파일 삭제
    `pickup-common` 모듈로 코드를 이전한 후, 기존 `admin` 및 `pickup-server` 모듈에 남아있던 중복된 `entity`와 `repository` 파일들을 삭제하여 리팩토링을 완료합니다.

--- pickup-server/src/main/java/me/synology/hajubal/coins/respository/PointUrlCallLogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.synology.hajubal.coins.respository;
-
-import me.synology.hajubal.coins.entity.PointUrlCallLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PointUrlCallLogRepository extends JpaRepository<PointUrlCallLog, Long> {
-}
